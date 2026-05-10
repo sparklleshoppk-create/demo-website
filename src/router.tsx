b1d3becd76f9faf21a34cd07eed808b02af14870
sparklleshoppk-create <sparklle.shop.pk@gmail.com>
2026-05-11 02:58:09 +0500

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,1 +1,17 @@
 # Add your router.tsx content here
+import { QueryClient } from "@tanstack/react-query";
+import { createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+
+export const getRouter = () => {
+  const queryClient = new QueryClient();
+
+  const router = createRouter({
+    routeTree,
+    context: { queryClient },
+    scrollRestoration: true,
+    defaultPreloadStaleTime: 0,
+  });
+
+  return router;
+};
